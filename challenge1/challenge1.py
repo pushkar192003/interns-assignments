0180654bd42573a92302697577f86f3167985f14
pushkar raj <pushkarraj192003@gmail.com>
2026-02-25 15:10:44 +0530

    did file parsing challenge
    problems encounterd were solved
    problem 1: consecutive paranthesis )( or of  this type 5( were not treated as multiplicationn so re,sub is used to solve that
    replaced ^ with ** for python syntax sychronistaion

---
+++ challenge1/challenge1.py
@@ -0,0 +1,21 @@
+#file parsinng problem :evaluated  expressionns in ip.txt file line by line and saved output in the op.txt file
+import re
+def evaluate_expression(expr):
+ 
+    expr = expr.replace("^", "**")
+    expr = re.sub(r'(\d|\))\s*\(', r'\1*(', expr)
+    return eval(expr)
+
+def file_parsing(ip,op):
+    with open(ip , 'r') as input, open(op, 'w') as output:
+        for line in input:
+            if not line.strip():
+                continue
+            exp=line.split('=')[0].strip()
+            if exp:
+                result= evaluate_expression(exp)
+                print(f"{exp} = {result}")
+                output.write(f"{exp} = {result}\n")
+if __name__ =='__main__':
+
+    file_parsing('ip.txt','op.txt')
